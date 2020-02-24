3a5eefae94c81e50febacee50511ae42f78ca6e3
ben04.li <ben04.li@vipshop.com>
2020-02-24 16:19:38 +0800

    compile OpenJDK 8 in Ubuntu 14.04

---
+++ ubuntu1404_openjdk8/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+        int a = 1, b = 1;
+        System.out.println("1 + 1 = " + add(a, b));
+    }
+
+    public static int add (int a, int b) {
+         return a + b;
+    }
+}
